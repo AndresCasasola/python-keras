1f862d21f602f60d9750b720a5888774d2765f0e
AndresCasasola <andres97dominguez@gmail.com>
2019-05-31 12:38:58 +0200

    Rewrite snake_keras basing on python classes

--- pygame/snake_keras.py
+++ pygame/snake_keras.py
@@ -1,125 +1,133 @@
 
 import sys, pygame, random
+from neuralNet import neural_net
 
-# Start mixer with 22050Hz, 2 channels, 16 bit sample size
-# Init mixer before pygame, otherwise the buffer does not assign right
-pygame.mixer.init(frequency=48000, channels=2, size=16, buffer=512) #22050
-# Init pygame
-pygame.init()
-
-############### Init environment
-# Show 800x600 window
-width, height = 600, 450
-screen = pygame.display.set_mode([width, height])
-s_score = pygame.mixer.Sound("sounds/TP_Get_Rupee.wav")
-s_hit = pygame.mixer.Sound("sounds/HitSound.wav")
-# Change window name
-pygame.display.set_caption("Travieso snake")
-gameFont = pygame.font.Font("fonts/comic.ttf", 40)
-
-############### Variables
-snake_start_position = [width/5, height/2]
-food_start_position = [random.randint(25,width-25),random.randint(25,height-25)]
-start_speed = [1, 0] # X, Y
-score=0;
-speed = start_speed
-snake = pygame.image.load("images/snake.png")
-snakerect = snake.get_rect()
-food = pygame.image.load("images/diamond2_res.png")
-foodrect = food.get_rect()
-# Colors
+############### Definitions
 white = 255, 255, 255
 font = 200, 200, 200
 background = 60, 60, 60
 black = 0, 0, 0
 
-# Start position
-snakerect.move_ip(snake_start_position)
-last = foodrect.move_ip(food_start_position)
+class SmartSnake():
 
-############### Loop conditions
-run = True
-stop = False
+############### Attributes
+	width, height = 600, 450
+	score = 0
+	speed = (0, 0)	# (X, Y)
+	snake = pygame.image.load("images/snake.png")
+	snakerect = snake.get_rect()
+	food = pygame.image.load("images/diamond2_res.png")
+	foodrect = food.get_rect()
+	screen = pygame.display.set_mode([width, height])
 
-############### Start loop
-while run:
-	if stop == True:
-		pygame.time.delay(1*1000) # ms
-	
-	pygame.time.delay(5) # ms
-	# Capture events
-	for event in pygame.event.get():
-		# If QUIT event then exit
-		if event.type == pygame.QUIT: run = False
-
-	# Key events
-	keys = pygame.key.get_pressed()
-	if keys[pygame.K_UP] or keys[pygame.K_DOWN] or keys[pygame.K_LEFT] or keys[pygame.K_RIGHT]:
-		speed = [0, 0]
-	if keys[pygame.K_UP]:
-		speed[1] = -1;
-	if keys[pygame.K_DOWN]:
-		speed[1] = 1;
-	if keys[pygame.K_LEFT]:
-		speed[0] = -1;
-	if keys[pygame.K_RIGHT]:
-		speed[0] = 1;
-	if keys[pygame.K_p]:
-		if stop == True:
-			stop = False
-			
-	# Test colission
-	if snakerect.colliderect(foodrect):
-		if colliding == False:
-			score = score + 1;			
-			s_score.play()
-			#s_hit.play()
-			foodrect.move_ip(random.randint(25,width-25)-foodrect.x,random.randint(25,height-25)-foodrect.y)
-			colliding = True
-	else:
-		colliding = False
-
-	# Move snake
-	snakerect = snakerect.move(speed)
-
-	if snakerect.left < 0 or snakerect.right > width:
-		run = False
-	if snakerect.top < 0 or snakerect.bottom > height:
-		run = False
-
-############### Draw background, snake, food, score and refresh screen
-	screen.fill(background)
-	screen.blit(food, foodrect)
-	screen.blit(snake, snakerect)
-	scoreSurface = gameFont.render(str(score), 1, font)
-	screen.blit(scoreSurface, (30, 30))
-	pygame.display.flip()
-
-############### End loop
+############### Init class attributes
+	def __init__(self, epochs=10, batch_size=10, epsilon=1, gamma=.8):
+		self.epochs = epochs
+		self.batch_size = batch_size
+		self.epsilon = epsilon
+		self.gamma = gamma
+		#self.model = neural_net([15, 16])
+		self.experience = []
 
 ############### End screen
-screen.fill(background)
-endTextSurface = gameFont.render('- YOU DIED -', 1, font)
-endScoreSurface = gameFont.render('Score: ' + str(score), 1, font)
-exitButton = pygame.image.load("images/exit_button_black.png")
-exitButtonRect = exitButton.get_rect()
-screen.blit(endTextSurface, (width/2-(endTextSurface.get_width()/2), height/3-(endTextSurface.get_height()/2)))
-screen.blit(endScoreSurface, (width/2-(endScoreSurface.get_width()/2), height/2-(endScoreSurface.get_height()/2)))
-exitButtonRect.move_ip(width/2-(exitButtonRect.width/2), height/1.2-(exitButtonRect.height/2))
-screen.blit(exitButton, exitButtonRect)
-pygame.display.flip()
+	def end_screen(self):
+		self.screen.fill(background)
+		endTextSurface = self.gameFont.render('- YOU DIED -', 1, font)
+		endScoreSurface = self.gameFont.render('Score: ' + str(self.score), 1, font)
+		exitButton = pygame.image.load("images/exit_button_black.png")
+		exitButtonRect = exitButton.get_rect()
+		self.screen.blit(endTextSurface, (self.width/2-(endTextSurface.get_width()/2), self.height/3-(endTextSurface.get_height()/2)))
+		self.screen.blit(endScoreSurface, (self.width/2-(endScoreSurface.get_width()/2), self.height/2-(endScoreSurface.get_height()/2)))
+		exitButtonRect.move_ip(self.width/2-(exitButtonRect.width/2), self.height/1.2-(exitButtonRect.height/2))
+		self.screen.blit(exitButton, exitButtonRect)
+		pygame.display.flip()
+		# Wait for exit button pressed
+		exit = False
+		while exit == False:
+			pygame.event.wait()
+			m1, m2, m3 = pygame.mouse.get_pressed()
+			if m1 == True:
+				mpos = pygame.mouse.get_pos()
+				if exitButtonRect.collidepoint(mpos):
+					exit = True
 
-exit = False
-while exit == False:
-	pygame.event.wait()
-	m1, m2, m3 = pygame.mouse.get_pressed()
-	if m1 == True:
-		mpos = pygame.mouse.get_pos()
-		if exitButtonRect.collidepoint(mpos):
-			exit = True
+############### Test colission
+	def collide(self, snakerect, foodrect):
+		if snakerect.colliderect(foodrect):
+			return True
+		else:
+			return False
 
-#pygame.time.delay(2*1000) # ms
+############### Draw background, snake, food, score and refresh screen
+	def draw_screen(self, screen):
+		self.screen.fill(background)
+		self.screen.blit(self.food, self.foodrect)
+		self.screen.blit(self.snake, self.snakerect)
+		scoreSurface = self.gameFont.render(str(self.score), 1, font)
+		screen.blit(scoreSurface, (30, 30))
+		pygame.display.flip()
 
+	def start_game(self):
+
+		# Init pygame
+		pygame.init()
+
+		# Change window name and create gameFont
+		pygame.display.set_caption("Smart snake")
+		self.gameFont = pygame.font.Font("fonts/comic.ttf", 40)
+
+		## Start position and speed
+		snake_start_position = [self.width/5, self.height/2]
+		food_start_position = [random.randint(25,self.width-25),random.randint(25,self.height-25)]
+		start_speed = (1, 0) # X, Y
+		self.snakerect.move_ip(snake_start_position)
+		self.foodrect.move_ip(food_start_position)
+		speed = start_speed		
+		
+		## Start loop
+		run = True
+		while run:
+			pygame.time.delay(5) # ms
+			# Capture events
+			for event in pygame.event.get():
+				# If QUIT event then exit
+				if event.type == pygame.QUIT: run = False
+			# Key events
+			keys = pygame.key.get_pressed()
+			if keys[pygame.K_UP] or keys[pygame.K_DOWN] or keys[pygame.K_LEFT] or keys[pygame.K_RIGHT]:
+				speed = [0, 0]
+			if keys[pygame.K_UP]:
+				speed[1] = -1
+			if keys[pygame.K_DOWN]:
+				speed[1] = 1
+			if keys[pygame.K_LEFT]:
+				speed[0] = -1
+			if keys[pygame.K_RIGHT]:
+				speed[0] = 1
+		
+			if self.collide(self.snakerect, self.foodrect) == True:
+				self.score = self.score + 1			
+				self.foodrect.move_ip(random.randint(25,self.width-25)-self.foodrect.x,random.randint(25,self.height-25)-self.foodrect.y)
+
+			# Move snake
+			self.snakerect = self.snakerect.move(speed)
+			# Test frame collisions
+			if self.snakerect.left < 0 or self.snakerect.right > self.width:
+				run = False
+			if self.snakerect.top < 0 or self.snakerect.bottom > self.height:
+				run = False
+			
+			self.draw_screen(self.screen)
+			## End loop
+
+		
+		## Show end screen when game finish
+		self.end_screen()
+
+
+# Main
+snake = SmartSnake()
+snake.start_game()
 pygame.quit()
 
 
